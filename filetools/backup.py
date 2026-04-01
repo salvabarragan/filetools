919c38127f8895231a82e6d188f85ab93e141c6b
Salva <salvabarragantorres@gmail.com>
2026-04-01 16:22:49 +0200

    feat: Add folder backup functionality

--- filetools/backup.py
+++ filetools/backup.py
@@ -1,12 +1,28 @@
 import os
 
 
-def backup(file, where):
-    if not os.path.exists(file):
-        raise FileNotFoundError(f"El archivo origen no existe: {file}")
-    if not os.path.exists(where):
-        os.makedirs(where)
-    filename = os.path.basename(file)
-    dest_path = os.path.join(where, filename)
-    with open(file, "rb") as src, open(dest_path, "wb") as dst:
-        dst.write(src.read())
+def backup(where, file=None, folder=None):
+    if (file is None and folder is None) or (file and folder):
+        raise ValueError("You must specify either a file or a folder, but not both or neither.")
+    elif file:
+        if not os.path.exists(file):
+            raise FileNotFoundError(f"The file does not exist: {file}")
+        if not os.path.exists(where):
+            os.makedirs(where)
+        filename = os.path.basename(file)
+        dest_path = os.path.join(where, filename)
+        with open(file, "rb") as src, open(dest_path, "wb") as dst:
+            dst.write(src.read())
+    elif folder:
+        if not os.path.exists(folder):
+            raise FileNotFoundError(f"The folder does not exist: {folder}")
+        foldername = os.path.basename(folder)
+        if not os.path.exists(os.path.join(where, foldername)):
+            os.makedirs(os.path.join(where, foldername))
+        folder_files = os.listdir(folder)
+        for file in folder_files:
+            current_file = os.path.join(folder, file)
+            dest_path = os.path.join(where, foldername, file)
+            with open(current_file, "rb") as src, open(dest_path, "wb") as dst:
+                dst.write(src.read())
+                
